6e457f052a87e538d3ab98c44a070e9aef7f2dad
Clipper52c <98652785+Clipper52c@users.noreply.github.com>
2024-01-19 09:20:00 -0500

    Add files via upload

---
+++ InputFileStatusConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataManagement.Core.Domain._FundCode;
+using System.Data.Entity.ModelConfiguration;
+
+
+namespace DataManagement.Persistence.EntityConfigurations._FundCode
+{
+    internal class InputFileStatusConfiguration
+        : EntityTypeConfiguration<InputFileStatus>
+    {
+        public InputFileStatusConfiguration()
+        {
+            Property(c => c.Id)
+            .IsRequired();
+
+            Property(c => c.FileName)
+             .IsOptional()
+             .HasMaxLength(200);
+
+            Property(c => c.EmailCommentPickup)
+            .IsOptional()
+            .HasMaxLength(200);
+
+        }
+    }
+}
+
